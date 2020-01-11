1cda332bc2889119647e479e321f2b889b1d7ce8
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-01-11 11:17:06 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+class Post(models.Model): 
+	titulo = models.CharField(max_length=100)
+	conteudo = models.TextField()
+	data = models.DateTimeField(default=timezone.now)
+	autor = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.titulo
---
+++ webapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from blog.views import PostListView, PostDetailView
+from blog import views
+
+urlpatterns = [
+	path('', PostListView.as_view(), name='blog-index'),
+	path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+	path('search/', views.search, name='search'),
+    path('admin/', admin.site.urls),
+]
